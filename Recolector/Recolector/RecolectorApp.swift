f7dbb6b36fdb3c75811e16174c330fa337875950
GReyes4 <A00833852@tec.mx>
2023-09-06 11:08:51 -0600

    BYBYUBUYB

---
+++ Recolector/Recolector/RecolectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecolectorApp.swift
+//  Recolector
+//
+//  Created by Alumno on 06/09/23.
+//
+
+import SwiftUI
+
+@main
+struct RecolectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
